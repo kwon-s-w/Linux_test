f07d19041f24c84c562c8dab7b2a325b2f7f7c64
kwon-s-w <kwon1745@naver.com>
2023-09-10 19:37:06 +0900

    Initial commit

---
+++ src/main/java/com/example/linux_test/LinuxTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.linux_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/linux_test/LinuxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linux_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
